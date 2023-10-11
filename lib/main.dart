3a2615138135de1234cc7cf26dfb1adf39883c67
Mateusz Baryla <m.baryla@protonmail.ch>
2023-10-11 22:38:14 +0200

    Estilo global para la aplicacion implementado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/configs/themes/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 6).theme(),
       home: Scaffold(
           appBar: AppBar(
             title: const Text('Material App Bar'),
